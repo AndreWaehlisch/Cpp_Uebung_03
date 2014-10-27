7f0e287b07331a433473834ef1c3e94b0afba011
AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>
2014-10-28 00:25:42 +0100

    initial commit
    Signed-off-by: AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>

---
+++ aufg1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Cluster
+{
+	public:
+	
+	double mstars; // Masse der Sterne
+	double mtotal; // Gesamtmasse
+	double eps; // Sternenstehungseffiziens
+	
+	Cluster(double iMtotal, double iEps)
+	{
+		mstars = 0.;
+		mtotal = iMtotal;
+		eps = iEps;
+	}
+	
+	double f() // Anteil der Sternmasse
+	{
+		return ( mstars / mtotal ) * 100;
+	}
+	
+	void starform()
+	{
+		mstars += eps * ( mtotal - mstars );
+	}
+};
+
+int main()
+{
+	Cluster A( 1e8, 0.3 );
+	Cluster B( 1e9, 0.1 );
+
+	for ( int i = 0; i < 15; i++ )
+	{
+		A.starform();
+		B.starform();
+	}
+
+	cout << "Anteil der Sternmasse (in Sonnenmassen) von Cluster A = " << A.f()  << "%" << endl;
+	cout << "Anteil der Sternmasse (in Sonnenmassen) von Cluster B = " << B.f()  << "%" << endl;
+	
+	return 0;
+}
---
+++ aufg2.cpp
@@ -0,0 +1,64 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Vektor
+{
+	public:
+	
+	double x, y, z;
+	
+	Vektor()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	
+	Vektor(double ix, double iy, double iz)
+	{
+		x = ix;
+		y = iy;
+		z = iz;
+	}
+	
+	double betrag()
+	{
+		return sqrt( x*x + y*y + z*z );
+	}
+	
+	friend ostream& operator<< (ostream& out, const Vektor& v)
+	{
+		out << "[ " <<  v.x << ", " << v.y << ", " << v.z << " ]";
+		return out;
+	}
+};
+
+double skalar(Vektor& a, Vektor& b)
+{
+	return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+Vektor kreuz(Vektor& a, Vektor& b)
+{
+	Vektor ergebnis;
+
+	ergebnis.x = ( a.y*b.z - a.z*b.y );
+	ergebnis.y = ( a.z*b.x - a.x*b.z );
+	ergebnis.z = ( a.x*b.y - a.y*b.x );
+	
+	return ergebnis;
+}
+
+int main()
+{
+	Vektor u(2, 1, -1);
+	Vektor v(1, 3, 5);
+	Vektor w = kreuz(u, v);
+	
+	cout << "Skalarprodukt von u und v: " << skalar(u, v) << endl; // Das Skalarprodukt verschwindet, u und v sind somit senkrecht.
+	cout << "Senkrecht auf u und v steht folgender Vektor: " << w << endl; // Der Vektor [8, -11, 5] ist das Ergebnis des Kreuzprodukts.
+	cout << "Dieser hat den Betrag: " << w.betrag() << endl; // Sein Betrag ist 14.4914
+	
+	return 0;
+}
