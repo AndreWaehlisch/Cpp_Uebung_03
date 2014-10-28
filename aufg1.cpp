34002e06f63c88d850d765b3826a6c3404957208
AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>
2014-10-28 16:04:52 +0100

    added .cpp~ files to gitignore
    Signed-off-by: AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>

--- aufg1.cpp
+++ aufg1.cpp
@@ -4,43 +4,44 @@ using namespace std;
 
 class Cluster
 {
-	public:
-	
-	double mstars; // Masse der Sterne
-	double mtotal; // Gesamtmasse
-	double eps; // Sternenstehungseffiziens
-	
-	Cluster(double iMtotal, double iEps)
-	{
-		mstars = 0.;
-		mtotal = iMtotal;
-		eps = iEps;
-	}
-	
-	double f() // Anteil der Sternmasse
-	{
-		return ( mstars / mtotal ) * 100;
-	}
-	
-	void starform()
-	{
-		mstars += eps * ( mtotal - mstars );
-	}
+public:
+
+  double mstars;		// Masse der Sterne
+  double mtotal;		// Gesamtmasse
+  double eps;			// Sternenstehungseffiziens
+
+    Cluster (double iMtotal, double iEps)
+  {
+    mstars = 0.;
+    mtotal = iMtotal;
+    eps = iEps;
+  }
+
+  double f ()			// Anteil der Sternmasse
+  {
+    return (mstars / mtotal) * 100;
+  }
+
+  void starform ()
+  {
+    mstars += eps * (mtotal - mstars);
+  }
 };
 
-int main()
+int
+main ()
 {
-	Cluster A( 1e8, 0.3 );
-	Cluster B( 1e9, 0.1 );
+  Cluster A (1e8, 0.3);
+  Cluster B (1e9, 0.1);
 
-	for ( int i = 0; i < 15; i++ )
-	{
-		A.starform();
-		B.starform();
-	}
+  for (int i = 0; i < 15; i++)
+    {
+      A.starform ();
+      B.starform ();
+    }
 
-	cout << "Anteil der Sternmasse (in Sonnenmassen) von Cluster A = " << A.f()  << "%" << endl;
-	cout << "Anteil der Sternmasse (in Sonnenmassen) von Cluster B = " << B.f()  << "%" << endl;
-	
-	return 0;
+  cout << "Anteil der Sternmasse (in Sonnenmassen) von Cluster A = " << A.f () << "%" << endl;
+  cout << "Anteil der Sternmasse (in Sonnenmassen) von Cluster B = " << B.f () << "%" << endl;
+
+  return 0;
 }
